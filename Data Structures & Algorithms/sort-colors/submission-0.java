69b4a3640bdf470a3cba31ead4211c15df5d801a
Vikas_Singh <vikkassingh978@gmail.com>
2026-07-04 22:25:10 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        mergeSort(nums,0,nums.length-1);
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+    }
+    private void mergeSort(int[] nums,int left,int right){
+        if(left>=right){
+            return;
+        }
+        int mid =left+(right-left)/2;
+        mergeSort(nums,left,mid);
+        mergeSort(nums,mid+1,right);
+        merge(nums,left,mid,right);
+    }
+    private void merge(int[] nums,int left,int mid,int right){
+        int[] temp=new int[right-left+1];
+        int i=left;
+        int j=mid+1;
+        int k =0;
+        while(i<=mid && j<=right){
+            if(nums[i]<=nums[j]){
+                temp[k++]=nums[i++];
+            }else{
+                temp[k++]=nums[j++];
+            }
+        }
+        while(i<=mid){
+            temp[k++]=nums[i++];
+        }
+        while(j<=right){
+            temp[k++]=nums[j++];
+        }
+        for(int p=0;p<temp.length;p++){
+            nums[left+p]=temp[p];
+        }
+    }
+}
